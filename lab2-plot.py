32fde163bb83b8b8c155997393b30f691c4c013f
Mateusz Magda <realgs@gmail.com>
2019-02-13 12:29:42 +0100

    Add a input/plot task to lab2

---
+++ lab2-plot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+x_knots = np.linspace(-3*np.pi,3*np.pi,201)
+y_knots = np.linspace(-3*np.pi,3*np.pi,201)
+X, Y = np.meshgrid(x_knots, y_knots)
+R = np.sqrt(X**2+Y**2)
+Z = np.cos(R)**2*np.exp(-0.3*R)
+ax = Axes3D(plt.figure(figsize=(8,5)))
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.cm.coolwarm, linewidth=0.4)
+plt.show()
--- lab2.py
+++ lab2.py
@@ -30,8 +30,10 @@ print(xIsEvenLog)
 #0 use alternative way of reading inputs - using library (0.5p)
 #1 perimeter & field of circles with radius X & Y. (1p)
 #2 find X & Y that satisfy: X is divisible by Y and both X & Y are even. (0.5p)
-#3 check if X is divisible by Y (do it in one line of code), print 'X is divisible by Y' or 'X is not divisible by Y'. (2p)
+#3 check if X is divisible by Y (do it in one line of code), print 'X is divisible by Y' or 'X is not divisible by Y'. (1p)
 #Ad 3 Hint- use the "ternary operator" as we did in calculating xIsEvenLog above.
 #4 Add rounding for the above x/y operation. Round to 2 decimal points. Hint: look up in Google "python limiting number of decimals". (1p)
-#5 Test your code. Check various edge cases. In other words: does your program (1, 3 & 4)work for all input values?
+#5 Look at lab2-plot.py and create your own script which takes a number as an input and plots the same 3D wave but with different characteristics
+# it's totally up to your imagination how do you want to amend the figure according to the input number (1p)
+#6 Test your code. Check various edge cases. In other words: does your program (1, 3, 4 & 5)work for all input values?
 # In case of task 4  do not forget to round to different amount of decimals and see if it still works.(3p)
